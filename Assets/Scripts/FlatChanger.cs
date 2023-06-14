694c462e6c9116a8c297e1dfacb8c9aa511f83d4
RobinHecate <124266311+RobinHecate@users.noreply.github.com>
2023-06-14 23:39:57 +0200

    Hannah Changes Floor

--- Assets/Scripts/FlatChanger.cs
+++ Assets/Scripts/FlatChanger.cs
@@ -6,25 +6,40 @@ public class FlatChanger : MonoBehaviour
 {
     public GameObject FirstFloor;
     public GameObject SecondFloor;
+    public GameObject secondFloorRoom;
+    public GameObject firstFloorRoom;
+
+    public GameObject hannah;
 
     bool floorChanged;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            switch (floorChanged)
-            {
-                case false:
-                    FirstFloor.SetActive(false);
-                    SecondFloor.SetActive(true);
-                    floorChanged = true;
-                    break;
-                case true:
-                    FirstFloor.SetActive(true);
-                    SecondFloor.SetActive(false);
-                    floorChanged = false;
-                    break;
-            }
+            StartCoroutine(ToSecondFloor());
         }
+    }
+
+    IEnumerator ToSecondFloor()
+    {
+        hannah.SetActive(false);
+        switch (floorChanged)
+        {
+            case false:
+                FirstFloor.SetActive(false);
+                SecondFloor.SetActive(true);
+                hannah.transform.position = secondFloorRoom.transform.position;
+                floorChanged = true;
+                break;
+            case true:
+                FirstFloor.SetActive(true);
+                SecondFloor.SetActive(false);
+                hannah.transform.position = firstFloorRoom.transform.position;
+                floorChanged = false;
+                break;
+        }
+        yield return new WaitForSeconds(.1f);
+        hannah.SetActive(true);
+
     }
 }
