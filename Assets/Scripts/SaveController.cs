1158e80f45e587a9e8dbd96989d6e2038b3493ef
RobinHecate <124266311+RobinHecate@users.noreply.github.com>
2023-06-15 12:25:47 +0200

    Final Touches

--- Assets/Scripts/SaveController.cs
+++ Assets/Scripts/SaveController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class SaveController : MonoBehaviour
@@ -10,24 +11,78 @@ public class SaveController : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI volumeText = null;
 
-    private static SaveController instance;
     private bool isFullscreen = false;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+    [SerializeField] private bool isMainMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private ThirdPersonCamera tpCam;
+    [SerializeField] private YawController yController;
+    [SerializeField] private Animator anim;
+    [SerializeField] private AudioSource audio;
     private void Start()
     {
         LoadValues();
+    }
+
+    private void Update()
+    {
+        if (!isMainMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isPaused)
+                    Resume();
+                else
+                    Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        anim.Play("DownMove");
+        BasicCharacterStateMachine.instance.enabled = true;
+        tpCam.enabled = true;
+        yController.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Back()
+    {
+        settingsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void ToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Settings()
+    {
+        settingsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        anim.Play("UpMove");
+        audio.Play();
+        BasicCharacterStateMachine.instance.enabled = false;
+        tpCam.enabled = false;
+        yController.enabled = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void VolumeSlider(float volume)
